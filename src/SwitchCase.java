e7b7eeca4550b92e25680f7a2b66af1aa642c3ee
Phuwadech Panichayasopa <aofphuwadech@gmail.com>
2024-03-25 20:53:11 +0700

    Swithcase

---
+++ src/SwitchCase.java
@@ -0,0 +1,15 @@
+class SwitchCase{
+    public static void main(String[] args) {
+        int x = 2;
+        switch (x){
+            case 1 :
+                System.out.println("this is 1");
+                break;
+            case 2 :
+                System.out.println("this is 2");
+                break;
+            default:
+                System.out.println("not is 1,2");
+        }
+    }
+}
